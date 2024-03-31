f6f1f4597c4649666c002ec24a1ef28901ac7eac
jhyunleehi <jhyunleehi@naver.com>
2024-03-31 23:43:09 +0900

    line plot

--- D3js/GetStart/my-app/src/index.js
+++ D3js/GetStart/my-app/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AppLine from './AppLine';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+      <Home />
+      <App />
+      <AppLine/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
